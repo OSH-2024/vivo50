e2359862156e244edb81e84db18cacb1cfee2920
pocket246 <fmr1733185241@163.com>
2024-05-06 07:19:19 -0700

    simple js server

---
+++ docs/nginx/js/node1.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+
+// 添加跨域支持
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
+// 定义路由
+app.get('/', (req, res) => {
+  res.send('Hello, world! This is from Node.js server.port:8002');
+});
+
+// 监听端口
+const PORT = 8001;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
+
