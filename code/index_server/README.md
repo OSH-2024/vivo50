89d7422f9773cfb2ee3f985ee8bb32d415529386
Mamya <3284727604@qq.com>
2024-05-28 23:13:45 +0800

    figue out how the vector works and the next we want to complete

--- code/index_server/README.md
+++ code/index_server/README.md
@@ -21,3 +21,18 @@ OSError: We couldn't connect to 'https://huggingface.co' to load this file, coul
 尝试远程打开dsw，感觉远程有点复杂，先放个[链接](#https://blog.csdn.net/Zheng113/article/details/135351718)。
 - 另一种连接思路：进入bin文件夹，运行`./neo4j`，然后运行`./cypher-shell`，可以终端操作neo4j，设置密码为`oshvivo50`，用户为`neo4j`，然后再次运行tag文件，即可登录。
 - 从文件读数据时，SimpleDirectoryReader貌似只能从文件夹读取
+### 记录
+调用load_data会自动判断文件类型，并且有个初始化id，包括以下内容
+> ImageDocument(id_='af11e34b-d33b-4ede-9e97-e41343d3516a', embedding=None, metadata={'file_path': '/mnt/workspace/temp/image-2.png', 'file_name': 'image-2.png', 'file_type': 'image/png', 'file_size': 67148, 'creation_date': '2024-05-28', 'last_modified_date': '2024-05-28'}, excluded_embed_metadata_keys=['file_name', 'file_type', 'file_size', 'creation_date', 'last_modified_date', 'last_accessed_date'], excluded_llm_metadata_keys=['file_name', 'file_type', 'file_size', 'creation_date', 'last_modified_date', 'last_accessed_date'], relationships={}, text='', start_char_idx=None, end_char_idx=None, text_template='{metadata_str}\n\n{content}', metadata_template='{key}: {value}', metadata_seperator='\n', image=None, image_path='/mnt/workspace/temp/image-2.png', image_url=None, image_mimetype=None, text_embedding=None),
+
+> Document(id_='b0fbd59b-a7f4-4201-ac8f-e04b6baad4f7', embedding=None, metadata={'file_path': '/mnt/workspace/temp/tag.ipynb', 'file_name': 'tag.ipynb', 'file_size': 1799, 'creation_date': '2024-05-27', 'last_modified_date': '2024-05-26'}, excluded_embed_metadata_keys=['file_name', 'file_type', 'file_size', 'creation_date', 'last_modified_date', 'last_accessed_date'], excluded_llm_metadata_keys=['file_name', 'file_type', 'file_size', 'creation_date', 'last_modified_date', 'last_accessed_date'], relationships={}, text='\n\n\nfrom llama_index.core import ServiceContext, SimpleDirectoryReader, StorageContext, Settings\nfrom llama_index.embeddings.huggingface import HuggingFaceEmbedding\n\n\n# ', start_char_idx=None, end_char_idx=None, text_template='{metadata_str}\n\n{content}', metadata_template='{key}: {value}', metadata_seperator='\n'), 
+
+以下代码实现在neo4j中存储数据
+```python
+    documents = SimpleDirectoryReader(filePath).load_data()
+    # 需要对文件进行分块和提取元数据操作，分为文本和图片
+    #以下将自动进行分块index
+    neo4j_vector = Neo4jVectorStore(username, password, url, embed_dim)
+    storage_context = StorageContext.from_defaults(vector_store=neo4j_vector)
+```
+
--- code/index_server/tagging.py
+++ code/index_server/tagging.py
@@ -13,26 +13,62 @@ username = "neo4j"
 password = "oshvivo50"
 url = "neo4j://localhost"
 embed_dim = 1536
-PATH = "/mnt/workspace/temp"
-# 登录neo4j，
-neo4j_vector = Neo4jVectorStore(username, password, url, embed_dim)
-# print(neo4j_vector)
-# load documents
-documents = SimpleDirectoryReader(PATH).load_data()
-# print("document/n")
-print(documents)
-from llama_index.core import StorageContext
-# 设置embed大模型
+filePath = "/mnt/workspace/temp"
+#设置embeding模型
 Settings.embed_model = HuggingFaceEmbedding(model_name="BAAI/bge-small-en-v1.5")
-# 文本索引
-Settings.transformations = [SentenceSplitter(chunk_size=1024)]
-#为文件创建索引
-index = VectorStoreIndex.from_documents(
-    documents,
-    embed_model=Settings.embed_model,
-    transformations=Settings.transformations
-)
-print(f"index\n",index)
+
+# 希望可以对传来的数据进行向量化存储，并提取出元数据方便可视化，但用已知函数只能提取出一些属性
+# 后续应该向结点中添加text，relation，
+# 并应学习并尝试管道
+def loadData(filePath):
+    documents = SimpleDirectoryReader(filePath).load_data()
+    # 可能需要对文件进行分块和提取元数据操作，分为文本和图片
+    
+    #以下将自动进行分块index
+    neo4j_vector = Neo4jVectorStore(username, password, url, embed_dim)
+    storage_context = StorageContext.from_defaults(vector_store=neo4j_vector)
+    index = MultiModalVectorStoreIndex.from_documents(
+        documents, storage_context=storage_context, image_vector_store=image_store
+    )
+    print(index)
+    # 可以尝试自定义节点，以上是调用函数自动分块并存储
+
+    
+# documents = SimpleDirectoryReader(filePath).load_data()
+# print(documents)
+# print("\n")
+# # 需要对文件进行分块和提取元数据操作，分为文本和图片
+# #以下将自动进行分块index
+# neo4j_vector = Neo4jVectorStore(username, password, url, embed_dim)
+# print("neo4j\n")
+# print(neo4j_vector)
+# storage_context = StorageContext.from_defaults(vector_store=neo4j_vector) 
+# print("\n\n")
+# print(storage_context)
+# index = VectorStoreIndex.from_documents(
+#     documents, storage_context=storage_context
+# )
+
+# print(index)
+# 登录neo4j，
+# neo4j_vector = Neo4jVectorStore(username, password, url, embed_dim)
+# # print(neo4j_vector)
+# # load documents
+# documents = SimpleDirectoryReader(PATH).load_data()
+# # print("document/n")
+# print(documents)
+# from llama_index.core import StorageContext
+# # 设置embed大模型
+# Settings.embed_model = HuggingFaceEmbedding(model_name="BAAI/bge-small-en-v1.5")
+# # 文本索引
+# Settings.transformations = [SentenceSplitter(chunk_size=1024)]
+# #为文件创建索引
+# index = VectorStoreIndex.from_documents(
+#     documents,
+#     embed_model=Settings.embed_model,
+#     transformations=Settings.transformations
+# )
+# print(f"index\n",index)
 ##MultiModalVectorStoreIndex可以储存多模态vector
 # storage_context = StorageContext.from_defaults(vector_store=neo4j_vector)
 # print("storage\n")
