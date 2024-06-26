8b658aeed837307dc7a8a2a4fe182b1b6c2cb25a
liuchang <2289880191@qq.com>
2024-06-26 22:37:08 +0800

    add config.py

--- code/config.py
+++ code/config.py
@@ -2,7 +2,10 @@ class args():
     def __init__(self):
         self.set={
             # general
-            "absolute_path":"D:\\PycharmProjects\\NewDFS\\",
+            "download_path":"/home/liuchang/dfile",
+            "json_path":"/home/liuchang/testfs/web_server/static/test.json",
+            "upload_path":"/home/liuchang/upfile",
+            "storage_path":"/jfs",
             "split_char":"%$$%@#!#(*%^&%",
             "use_ray":False,
             "listen_ip":"0.0.0.0",
@@ -13,23 +16,14 @@ class args():
             "web_listen_central":10000,
             "web_send_central":3333,
             # central_server
-            "central_ip": "192.168.209.1",
+            "central_ip": "192.168.136.131",
             "central_listen_web":3333,
             "central_send_web":10000,
-            "central_listen_storage":6000,
-            "central_send_storage":8888,
-
             # Ray_Module
             "Ray_listen_neo":4001,
             "Ray_send_neo":4000,
-            # storage_server
-            "storage_num":2,
-            "storage_ip":["192.168.137.159"],
-            "storage_listen_central":[8888],
-            "storage_send_central":[6000],
             # neo_server
-            "neo_ip":"192.168.209.136",
+            "neo_ip":"192.168.136.131",
             "neo_listen_Ray":4000,
             "neo_send_Ray":4001,
 }
-
